c035c9bc730c42966b1a7782688817b6d7021f6c
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-01-15 18:02:52 -0300

    axios working with github devs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
@@ -8,21 +9,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-week-6sybr.mongodb
     useUnifiedTopology: true,
 });
 
-app.use(express.json()); // 'use' works for all kind of routes
-
-// HTTP Methods: get, post, put, delete
-
-// Parameter's types:
-    // query params: reqest.query
-            // (Filtros, ORdenação, Paginação...)
-    // route params: request.params
-            // (Identificar um recurso na alteração ou remoção)
-    // body: request.body
-            // (Dados para criação ou alteração de um registro)
-
-app.post('/users', (request, response) => {
-    console.log(request.body);
-    return response.json({ message: 'Hello Paulo OmniStack'});
-});
+app.use(express.json()); // 'use' works for all kind of routes + have to be before call them.
+app.use(routes);
 
 app.listen(3333);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,19 @@
 const { Router } = require('express');
+const axios = require('axios');
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-    console.log(request.body);
+routes.post('/devs', async (request, response) => {
+
+    const { github_username } = request.body;
+
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    
+    const { name = login, avatar,_url, bio } = apiResponse.data;
+
+    console.log(name, avatar_url, bio, github_username);
+
     return response.json({ message: 'Hello Paulo OmniStack'});
 });
+
+module.exports = routes;
