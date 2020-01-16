a105b6ca60fbe2985944eafabb461c03f7b6f7df
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-01-16 16:23:04 -0300

    class 2 done

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,19 +1,13 @@
 const { Router } = require('express');
-const axios = require('axios');
+
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
-    const { github_username } = request.body;
-
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    
-    const { name = login, avatar,_url, bio } = apiResponse.data;
-
-    console.log(name, avatar_url, bio, github_username);
-
-    return response.json({ message: 'Hello Paulo OmniStack'});
-});
+routes.get('/search', SearchController.index)
 
 module.exports = routes;
