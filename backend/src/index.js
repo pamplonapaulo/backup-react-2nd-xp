50fa9a618d5a5fa9caa66e1b4f52bb73b939b919
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-01-17 16:52:32 -0300

    end class 3

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-week-6sybr.mongodb
     useUnifiedTopology: true,
 });
 
+app.use(cors({ origin: 'http://localhost:3000' }));// without parameters would mean anyone can access.
 app.use(express.json()); // 'use' works for all kind of routes + have to be before call them.
 app.use(routes);
 
